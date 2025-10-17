9707a50c08c4bf40c83c975fd5c05dd847aa8772
Yeji Kim <yejikim.erica@gmail.com>
2025-10-18 07:17:13 +0900

    feat(StringCalculator): 문자열을 숫자 배열로 변환하는 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -12,4 +12,27 @@ public class StringCalculator {
         String regex = String.format("[%s]", DEFAULT_SEPARATORS);
         return text.split(regex);
     }
+
+    int[] convertToIntArray(String[] stringArray) {
+        int[] intArray = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            intArray[i] = convertToInt(stringArray[i]);
+        }
+        return intArray;
+    }
+
+    private int convertToInt(String letter) {
+        int number;
+        try {
+            number = Integer.parseInt(letter);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자로 변환할 수 없는 값이 포함되어 있습니다.");
+        }
+
+        if (number <= 0) {
+            throw new IllegalArgumentException("양수가 아닌 값이 포함되어 있습니다.");
+        }
+
+        return number;
+    }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
     private StringCalculator stringCalculator;
@@ -24,5 +25,58 @@ public class StringCalculatorTest {
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 문자배열을_숫자배열로_변환() {
+        // given
+        String[] testArray = {"1", "2", "3"};
+        int[] expected = {1, 2, 3};
+
+        // when
+        int[] result = stringCalculator.convertToIntArray(testArray);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 숫자가_아닌_문자_포함시_예외발생() {
+        // given
+        String[] testArray = {"1", "r", "3"};
+
+        // when & then
+        assertThatThrownBy(() -> stringCalculator.convertToIntArray(testArray))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 빈문자열_포함시_예외발생() {
+        // given
+        String[] testArray = {"", "2", "3"};
+
+        // when & then
+        assertThatThrownBy(() -> stringCalculator.convertToIntArray(testArray))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void zero_포함시_예외발생() {
+        // given
+        String[] testArray = {"0", "2", "3"};
+
+        // when & then
+        assertThatThrownBy(() -> stringCalculator.convertToIntArray(testArray))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음수_포함시_예외발생() {
+        // given
+        String[] testArray = {"1", "-1651", "3"};
+
+        // when & then
+        assertThatThrownBy(() -> stringCalculator.convertToIntArray(testArray))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
